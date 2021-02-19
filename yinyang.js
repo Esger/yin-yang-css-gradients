b17da0f699e24412821f4451a143e332c2ef2a90
Esger Jellema <esgerj@gmail.com>
2021-02-19 21:55:49 +0100

    roteren correct

--- yinyang.js
+++ yinyang.js
@@ -6,35 +6,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     };
 
     setGradients = (angle) => {
-        const centerX = '50vw';
+        const centerX = '50%';
         const centerY = '50vh';
-        // const size = '60vmin';
-        // const halfSize = '30vmin';
-        const quarterSize = 15;
-        const dotSize = 5;
+        const size = 60;
+        const halfSize = size / 2;
+        const quarterSize = size / 4;
+        const dotSize = size / 10;
+        foreGround = '#81d4fa';
 
         const dx = (angle) => {
-            return (Math.cos(Math.radians(angle)) * quarterSize).toFixed(2);
-        };
-        const dy = (angle) => {
             return (Math.sin(Math.radians(angle)) * quarterSize).toFixed(2);
         };
-        const blackDotX = '50%';
-        const blackDotY = 'calc(' + centerY + ' - ' + quarterSize + 'vmin)';
-        const whiteDotX = 'calc(50% + ' + dx(angle) + 'vmin)';
-        const whiteDotY = 'calc(' + centerY + ' + ' + dy(angle) + 'vmin)';
+        const dy = (angle) => {
+            return (Math.cos(Math.radians(angle)) * quarterSize).toFixed(2);
+        };
+        const blackDotX = 'calc(50vw + ' + dx(angle) + 'vmin)';
+        const blackDotY = 'calc(' + centerY + ' - ' + dy(angle) + 'vmin)';
+        const whiteDotX = '50vw';
+        const whiteDotY = 'calc(' + centerY + ' + ' + quarterSize + 'vmin)';
         const black = '#191919';
         const white = '#e6e6e6';
+        const front = 'radial-gradient(circle, transparent ' + halfSize + 'vmin, ' + foreGround + ' ' + halfSize + 'vmin 100%)';
+        let whiteHead = 'radial-gradient(circle at ' + blackDotX + ' ' + blackDotY + ', ' + black + ' 0 ' + dotSize + 'vmin, ' + white + ' 0 ' + quarterSize + 'vmin, transparent 0 100%)';
         let blackHead = 'radial-gradient(circle at ' + whiteDotX + ' ' + whiteDotY + ', ' + white + ' 0 ' + dotSize + 'vmin, ' + black + ' 0 ' + quarterSize + 'vmin, transparent 0 100%)';
-        let blackHalfRight = 'linear-gradient(' + angle + 'deg, transparent 0' + centerX + ', ' + black + ' ' + centerX + ' 100%)';
-        let whiteHead = 'radial-gradient(circle at ' + blackDotX + ' ' + blackDotY + ', ' + black + ' ' + dotSize + 'vmin, ' + white + ' 0 ' + quarterSize + 'vmin, transparent 0 100%)';
-        let whiteHalfLeft = 'linear-gradient(' + angle + 'deg, ' + white + ' 0 ' + centerX + ', transparent ' + centerX + ' 100%)';
-        const gradients = [blackHead, whiteHead, blackHalfRight, whiteHalfLeft]; //
+        let whiteHalfLeft = 'linear-gradient(' + (angle + 90) + 'deg, ' + white + ' 0 ' + centerX + ', transparent ' + centerX + ' 100%)';
+        let blackHalfRight = 'linear-gradient(90deg, transparent 0' + centerX + ', ' + black + ' ' + centerX + ' 100%)';
+        const gradients = [front, whiteHead, blackHead, blackHalfRight, whiteHalfLeft]; //front, 
         const backgroundImage = gradients.join(', ');
-        const html = document.getElementsByTagName('html')[0];
-        html.style.backgroundImage = backgroundImage;
-        console.log(backgroundImage);
+        const container = document.getElementsByTagName('body')[0];
+        container.style.backgroundImage = backgroundImage;
+        // console.log(backgroundImage);
     };
 
-    setGradients(99);
+    setGradients(30);
 });
