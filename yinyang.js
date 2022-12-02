870ae6effad157cf62b731e74b6d54004bfe4ef7
Esger Jellema <esgerj@gmail.com>
2022-12-02 15:22:53 +0100

    hoek input

--- yinyang.js
+++ yinyang.js
@@ -38,28 +38,69 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     setGradients = (angle) => {
 
-        const blackDotX = 'calc(50vw + ' + dx(angle) + 'vmin)';
-        const blackDotY = 'calc(' + centerY + ' - ' + dy(angle) + 'vmin)';
-        const whiteDotX = '50vw';
-        const whiteDotY = 'calc(' + centerY + ' + ' + quarterSize + 'vmin)';
+        const whiteHeadX = 'calc(50vw + ' + dx(angle) + 'vmin)';
+        const whiteHeadY = 'calc(' + centerY + ' - ' + dy(angle) + 'vmin)';
+        const whiteRightX = 'calc(' + dx(angle) + 'vmin)';
+        const whiteRightY = 'calc(' + centerY + ' - ' + dy(angle) + 'vmin)';
+        const blackHeadX = '50vw';
+        const blackHeadY = 'calc(' + centerY + ' + ' + quarterSize + 'vmin)';
         const front = 'radial-gradient(circle, transparent ' + halfSize + 'vmin, ' + foreGround + ' ' + halfSize + 'vmin 100%)';
-        const whiteHead = radialGradient(blackDotX, blackDotY, black, dotSize, white, quarterSize);
-        const blackHead = radialGradient(whiteDotX, whiteDotY, white, dotSize, black, quarterSize);
-        const whiteLeft = linearGradient(angle, white, centerX, 'transparent');
-        const whiteRight = linearGradient(angle, 'transparent', centerX, white);
-        const blackLeft = linearGradient(0, black, centerX, 'transparent');
-        const blackRight = linearGradient(0, 'transparent', centerX, black);
-        const phase0 = [whiteHead, blackHead, whiteLeft, blackRight, whiteRight, blackLeft,];
-        const phase1 = [whiteHead, blackHead, whiteLeft, whiteRight, blackRight, blackLeft,];
+        const whiteHead = {
+            gradient: radialGradient(whiteHeadX, whiteHeadY, black, dotSize, white, quarterSize),
+            position: '0',
+            size: '100%'
+        };
+        const blackHead = {
+            gradient: radialGradient(blackHeadX, blackHeadY, white, dotSize, black, quarterSize),
+            position: '0',
+            size: '100%'
+        };
+        const whiteLeft = {
+            gradient: linearGradient(angle, white, centerX, 'transparent'),
+            position: '0',
+            size: '100%'
+        };
+        const whiteRight = {
+            gradient: radialGradient(whiteRightX, whiteRightY, 'transparent', quarterSize, white, 50),
+            position: '100%',
+            size: '50%'
+        };
+        const blackLeft = {
+            gradient: radialGradient(blackHeadX, blackHeadY, 'transparent', quarterSize, black, 50),
+            position: '0',
+            size: '50%'
+        };
+        // const blackLeft = linearGradient(0, black, centerX, 'transparent');
+        const blackRight = {
+            gradient: linearGradient(0, 'transparent', centerX, black),
+            position: '0',
+            size: '100%'
+        };
+        const phase0 = [whiteRight];
+        // const phase0 = [whiteHead, blackHead, whiteLeft, blackRight, whiteRight, blackLeft,];
+        const phase1 = [front, whiteRight, blackLeft, whiteHead, whiteLeft, blackHead, blackRight,];
         const phase2 = [front, whiteHead, blackHead, whiteLeft, blackRight, whiteRight, blackLeft,];
         const phase3 = [whiteHead, blackHead, whiteLeft, blackRight, whiteRight, blackLeft,];
+
         const phases = [phase0, phase1, phase2, phase3];
         const phaseIndex = Math.floor((angle % 720) / 180);
-        const backgroundImage = phases[phaseIndex].join(', ');
+
+        const backgroundImage = phases[phaseIndex].map(item => item.gradient).join(', ');
+        const backgroundPosition = phases[phaseIndex].map(item => item.position).join(', ');
+        const backgroundSize = phases[phaseIndex].map(item => item.size).join(', ');
+
         const container = document.getElementsByTagName('body')[0];
         container.style.backgroundImage = backgroundImage;
+        container.style.backgroundPosition = backgroundPosition;
+        container.style.backgroundSize = backgroundSize;
+
         console.log(backgroundImage);
     };
 
-    setGradients(20);
+    setGradients(0);
+
+    const rangeInput = document.getElementsByClassName('angleInput')[0];
+    rangeInput.addEventListener('change', element => {
+        setGradients(parseInt(element.target.value));
+    });
 });
